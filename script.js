8e4db06334a744abe26c0e1de7e59305e305e95a
florian1881 <florian1881@gmail.com>
2021-06-26 23:20:20 +0200

    Tailwind - sound add - progressbar add

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 let counter;
 let intervalId = null;
+let originalCounter;
+let myProgressBar=document.getElementById("progressBar") ;
 
+function playAudio() {
+    document.getElementById("audio").play();
+}
+function progressBar(){
+    barWidth= counter*100/originalCounter;
+    myProgressBar.style.width=barWidth+"%";
+}
 function start() {
     // console.log(Number.isNaN(counter));
     // if (!Number.isNaN(counter)) {
     if (intervalId === null) {
         //Recupération de la valeur de l'utilisateur
         counter = document.getElementById("timing").value;
+        //keep orginal value for progress bar
+        originalCounter=counter;
         // Modif de l'html
         document.getElementById("bip").innerHTML = counter;
-        document.getElementById("btn").classList.remove("btn-primary");
         document.getElementById("btn").innerHTML = "Compteur en cours";
         //Set de l'intervalle d'execution setInterval(functionARejouer[function()],IntervalleDeRepetitionEnMs[Number])
         intervalId = setInterval(count, 1000);
@@ -17,23 +27,28 @@ function start() {
 }
 
 function count() {
-    if (counter == 0) {
+    if (counter == 1) {
+        counter = 0;
+        playAudio()
         finish();
         return
     };
-    counter--;   
+    counter--;
+    progressBar();
     document.getElementById("bip").innerHTML = counter;
     
 }
 
 function finish() {
-    clearInterval(intervalId);
+    
     document.getElementById("bip").innerHTML = counter;
+    clearInterval(intervalId);
     // Modif de l'html
-    document.getElementById("btn").classList.add("btn-primary");
-    document.getElementById("btn").classList.remove("btn-secondary");
+    myProgressBar.style.width=0+"%";
     document.getElementById("btn").innerHTML = "Relancer le compteur";
     counter = document.getElementById("timing").value;
+    //play the sound 
+    
     //Reset de l'intervalle
     intervalId = null;
 }
